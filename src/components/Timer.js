1f65b7b3511ac62201545ef614d2fa0f46093877
allentsangdev <allentsang04@gmail.com>
2023-03-07 01:17:59 -0500

    clean up file structure

---
+++ src/components/Timer.js
@@ -0,0 +1,13 @@
+// Use moment.js
+
+import React from 'react'
+
+function Timer() {
+    return(
+        <div className='timer'>
+            <p></p>
+        </div>
+    )
+}
+
+export default Timer
